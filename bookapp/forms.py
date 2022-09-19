967423b4f19e4927df15399f5eae3c5d0594155b
juniorrestituyo <juniorrestituyo@outlook.com>
2022-09-19 12:50:53 -0400

    Adding changes

--- bookapp/forms.py
+++ bookapp/forms.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from django.forms import ModelForm
 from .models import Book
 
@@ -5,4 +6,21 @@ from .models import Book
 class BookForm(ModelForm):
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = ["title", "author", "status", "checked"]
+        labels ={
+            "checked": "Checked out by"
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            # field.widget.attrs.update({"class": "form"})
+            self.fields["title"].widget.attrs.update({"class": "form-input", "placeholder": "Title"})
+            self.fields["author"].widget.attrs.update({"class": "form-input", "placeholder": "Author"})
+            self.fields["status"].widget.attrs.update({"class": "form-select"})
+            self.fields["checked"].widget.attrs.update({"class": "form-select"})
+
+
+            
+
