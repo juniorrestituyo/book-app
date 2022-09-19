5a9a2df90772351862dfeefd3dd507ea2a1494c4
juniorrestituyo <juniorrestituyo@outlook.com>
2022-09-19 16:32:47 -0400

    Deploying app

--- management/settings.py
+++ management/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4)xq^5pgy9ve0y+h)92!6$-&#c(0cfa(5pek)u=yq7cxtsb5g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "library-management-app03.herokuapp.com"]
 
 
 # Application definition
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
