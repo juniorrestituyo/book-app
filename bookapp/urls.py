3d483d09b9fbaad8da3fb9303bc3f2dff14b3c1d
juniorrestituyo <juniorrestituyo@outlook.com>
2022-09-19 10:06:08 -0400

    Adding changes

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.pageBooks, name="pageBooks")
+    path("", views.mainPage, name="mainPage"),
+    path("add/", views.addBook, name="add-book"),
+    path("edit/", views.editBook, name="edit-book"),
+    path("delete/", views.deleteBook, name="delete-book"),
 ]
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
 
-def pageBooks(request):
+def mainPage(request):
     return render(request, "bookapp/books.html")
 
+
+def addBook(request):
+    page = "add"
+    context = {
+        "page": page,
+    }
+    return render(request, "bookapp/add_edit.html", context)
+
+
+def editBook(request):
+    page = "edit"
+    context = {
+        "page": page,
+    }
+    return render(request, "bookapp/add_edit.html", context)
+
+
+def deleteBook(request):
+    return render(request, "bookapp/delete.html")
